595aebc2e1935bace36171c1fc5d86a2c0906b72
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2021-11-06 14:22:46 -0300

    Update fintech_web_app_deploy.py

--- fintech_web_app_deploy.py
+++ fintech_web_app_deploy.py
@@ -43,7 +43,7 @@ st.subheader('Simulação')
 st.header('Easy Bank')
 
 renda = st.number_input('Me informe sua renda.')
-idade = st.number_input('Agora, preciso saber da sua idade.', value_min=18)
+idade = st.number_input('Agora, preciso saber da sua idade.', 18)
 emprestimo = st.number_input('Qual o valor de empréstimo do seu interesse?')
 st.markdown('Sem juros')
 parcela = st.radio('Selecione a quantidade de parcela',(1, 2, 3, 4, 5, 6))
