2cf3135f1f0ecb0fc8019c649d78c109f2ec2c4e
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2021-11-07 14:24:19 -0300

    Update fintech_web_app_deploy.py

--- fintech_web_app_deploy.py
+++ fintech_web_app_deploy.py
@@ -28,8 +28,8 @@ st.sidebar.markdown('Feito por : Bruno Rodrigues Carloto')
 
 st.sidebar.markdown("Redes Sociais :")
 st.sidebar.markdown("- [Linkedin](https://www.linkedin.com/in/bruno-rodrigues-carloto)")
-st.sidebar.markdown("- [Medium](https://https://br-cienciadedados.medium.com)")
-st.sidebar.markdown("- [Github](https://https://github.com/brunnosjob)")
+st.sidebar.markdown("- [Medium](https://br-cienciadedados.medium.com)")
+st.sidebar.markdown("- [Github](https://github.com/brunnosjob)")
 
 if pag == 'Interagir com a inteligência':
     
