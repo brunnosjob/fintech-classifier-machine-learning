40b1f82ef6f75671c7dba5940c8d81ac879ec52f
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2023-12-09 14:46:20 -0300

    Update fintech_web_app_deploy.py

--- fintech_web_app_deploy.py
+++ fintech_web_app_deploy.py
@@ -25,12 +25,12 @@ st.markdown('*__Observação: para mais informações acerca do projeto, clique 
 foto = Image.open('bruno.carloto (2).png')
 st.sidebar.image(foto, use_column_width=True)
 st.sidebar.subheader('Bruno Rodrigues Carloto')
-st.sidebar.markdown('Analista de Inteligência de Mercado de Crédito')
+st.sidebar.markdown('Analista de dados')
 st.sidebar.markdown('#### Projeto de portfólio de Ciência de Dados')
 st.sidebar.markdown('''Leia o [artigo do projeto](https://br-cienciadedados.medium.com/projeto-de-machine-learning-ii-9c889faec8df), o qual descreve o passo a passo
 do desenvolvimento do modelo de machine learning. As descrições vão desde a limpeza dos dados até à análise do desempenho dos modelos e a seleção do melhor.''')
 st.sidebar.title('Menu')
-pag = st.sidebar.selectbox('Selecione a página', ['Interagir com a inteligência', 'Sobre o projeto', 'Dashboard da base de dados do projeto'])
+pag = st.sidebar.selectbox('Selecione a página', ['Interagir com a inteligência', 'Dashboard da base de dados do projeto'])
 
 st.sidebar.markdown('Feito por : Bruno Rodrigues Carloto')
 
